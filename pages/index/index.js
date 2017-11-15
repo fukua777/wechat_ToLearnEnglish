a9ebe9d80de62e0fc4c463c667839d9642d5c9ef
fukua777 <fukua_777@163.com>
2017-11-15 20:45:50 +0800

    add_api

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,19 +1,59 @@
 //index.js
 //获取应用实例
-var app = getApp()
+var app = getApp();
+
+var md5 = require('../../src/js/md5.js');
+
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    inputValue: '',
+    userInfo: {},
+    url: 'http://openapi.youdao.com/api',
+    appKey: '756f2b3ab40cf4be',
+    key: 'F6UbvQw8DN2d3tXnLvjg8hCQXkFBXynA',
+    salt: Math.floor(Math.random() * 10),
+    explains: ''
+  },
+  requesFunc: function(req){
+    if(!req){
+      return;
+    }
+    console.log(this);
+    var that = this;
+    var explains = this.data.explains;
+    wx.request({
+      url: this.data.url,
+      data: {
+        'q': req,
+        'from': 'EN',
+        'to': 'zh_CHS',
+        'appKey': this.data.appKey,
+        'salt': this.data.salt,
+        'sign': md5(this.data.appKey + req + this.data.salt + this.data.key).toUpperCase()
+      },
+      method: 'GET',
+      dataType: 'json',
+      success: function(res) {
+        that.setData({
+          explains: res.data.basic.explains || ''
+        });
+      },
+      fail: function(err) {
+        console.log(err);
+      },
+    });
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  search: function(e){
+    if(e.type == "input"){
+      this.setData({
+        inputValue: e.detail.value
+      });
+    } else if(e.type == "tap"){
+      this.requesFunc(this.data.inputValue);
+    }
   },
   onLoad: function () {
-    console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
